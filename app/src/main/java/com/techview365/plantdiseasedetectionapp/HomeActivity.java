2279b40465f5a1c66da97d56a960dcf9e100db7c
Manish <manish.dinkar13@gmail.com>
2024-02-29 00:28:04 +0530

    Fragment Design completed

--- app/src/main/java/com/techview365/plantdiseasedetectionapp/HomeActivity.java
+++ app/src/main/java/com/techview365/plantdiseasedetectionapp/HomeActivity.java
@@ -4,42 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
-    FirebaseAuth auth;
-    Button button;
-    TextView textView;
-    FirebaseUser user;
+    BottomNavigationView bottomNavigationView;
+    CropFragment cropFragment = new CropFragment();
+    CommunityFragment communityFragment = new CommunityFragment();
+    ExploreFragment exploreFragment = new ExploreFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
-        user = auth.getCurrentUser();
-        if(user == null){
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }else{
-            textView.setText(user.getEmail());
-        }
-        button.setOnClickListener(new View.OnClickListener() {
+
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,cropFragment).commit();
+
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.crop) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, cropFragment).commit();
+                    return true;
+                } else if (itemId == R.id.community) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, communityFragment).commit();
+                    return true;
+                } else if (itemId == R.id.explore) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, exploreFragment).commit();
+                    return true;
+                } else if (itemId == R.id.profile) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                    return true;
+                }
+
+                return false;
             }
         });
+
     }
 }
